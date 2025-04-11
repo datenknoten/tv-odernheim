3270ce86484d0c2ed35739dc03b085eaed552e07
Tim Schumacher <tim@datenknoten.me>
2025-04-11 22:31:31 +0200

    feat: extract config

---
+++ src/config/site.ts
@@ -0,0 +1,28 @@
+export const siteConfig = {
+	name: "Turnverein Odernheim am Glan 1890 e.V.",
+	shortName: "TV Odernheim",
+	address: {
+		street: "Turnhallstraße 6",
+		city: "Odernheim am Glan",
+		zip: "55571",
+		country: "Deutschland",
+	},
+	contact: {
+		phone: "+49 123 456789",
+		email: "horizonte@tv-odernheim.de",
+	},
+	legal: {
+		register: {
+			court: "Amtsgericht Bad Kreuznach",
+			number: "VR 667",
+		},
+		vatId: "DE 123 456 789",
+		responsiblePerson: {
+			name: "Kristen Hartmann",
+			position: "2. Vorsitzende",
+		},
+	},
+	social: {
+		instagram: "https://instagram.com/tv_odernheim/",
+	},
+};
